b4c7348c25371b78a4f2293cb2a9a734feaddef9
ayushajain <ayushajain@gmail.com>
2015-06-24 23:05:09 -0700

    changed background

--- MainJS.js
+++ MainJS.js
@@ -1,8 +1,24 @@
 var h = $(window).height();
 var w = $(window).width();
 
+
 $("svg").attr("width", w);
 $("svg").attr("height", h);
+
+$("#test").hover(
+	function(){
+		var opacity = $(this).css("opacity");
+		if(opacity > 0.5){
+			$(this).attr("r", 40).fadeIn();
+		}
+	},
+	function(){
+		var opacity = $(this).css("opacity");
+		if(opacity > 0.5){
+			$(this).attr("r", 30).fadeIn();
+		}
+	}
+);
 
 $(window).on('resize', function(){
 	h = $(window).height();
@@ -10,3 +26,4 @@ $(window).on('resize', function(){
 	$("svg").attr("width", w);
 	$("svg").attr("height", h);
 });
+
