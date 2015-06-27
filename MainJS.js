697a3aacaa0ce09d3eab1b4488cbfb793f4ed63b
ayushajain <ayushajain@gmail.com>
2015-06-26 23:46:36 -0700

    projects

--- MainJS.js
+++ MainJS.js
@@ -7,6 +7,8 @@ $("svg").attr("height", h);
 
 //fade out divs
 $("#Proj1PreInfo").fadeOut();
+$("#Proj2PreInfo").fadeOut();
+$("#Proj3PreInfo").fadeOut();
 
 
 $("#P1Hover").hover(
@@ -14,7 +16,7 @@ $("#P1Hover").hover(
 		var opacity = $("#Proj1C").css("opacity");
 		if(opacity > 0.5){
 			$("#Proj1C").attr("r", 35);
-			$("#Proj1PreInfo").fadeIn();
+			$("#Proj1PreInfo").stop().fadeIn();
 			$(this).attr("x", -500);
 			$(this).attr("width", 690);
 			$(this).attr("height", 880);
@@ -22,15 +24,56 @@ $("#P1Hover").hover(
 	},
 	function(){
 		var opacity = $("#Proj1C").css("opacity");
-		if(opacity > 0.5){
-			$("#Proj1C").attr("r", 30);
-			$("#Proj1PreInfo").fadeOut();
-			$(this).attr("x", -31);
-			$(this).attr("width", 60);
-			$(this).attr("height", 60);
-		}
+		$("#Proj1C").attr("r", 30);
+		$("#Proj1PreInfo").stop().fadeOut();
+		$(this).attr("x", -31);
+		$(this).attr("width", 60);
+		$(this).attr("height", 60);
 	}
 );
+$("#P2Hover").hover(
+	function(){
+		var opacity = $("#Proj2C").css("opacity");
+		if(opacity > 0.5){
+			$("#Proj2C").attr("r", 35);
+			$("#Proj2PreInfo").stop().fadeIn();
+			$(this).attr("x", -200);
+			$(this).attr("width", 690);
+			$(this).attr("height", 880);
+		}
+	},
+	function(){
+		var opacity = $("#Proj2C").css("opacity");
+		$("#Proj2C").attr("r", 30);
+		$("#Proj2PreInfo").stop().fadeOut();
+		$(this).attr("x", -31);
+		$(this).attr("width", 60);
+		$(this).attr("height", 60);
+	}
+);
+$("#P3Hover").hover(
+	function(){
+		var opacity = $("#Proj3C").css("opacity");
+		if(opacity > 0.5){
+			$("#Proj3C").attr("r", 35);
+			$("#Proj3PreInfo").stop().fadeIn();
+			$(this).attr("x", -310);
+			$(this).attr("y", -840);
+			$(this).attr("width", 690);
+			$(this).attr("height", 880);
+		}
+	},
+	function(){
+		var opacity = $("#Proj3C").css("opacity");
+		$("#Proj3C").attr("r", 30);
+		$("#Proj3PreInfo").stop().fadeOut();
+		$(this).attr("x", -31);
+		$(this).attr("y", -30)
+		$(this).attr("width", 60);
+		$(this).attr("height", 60);
+	}
+);
+
 
 $(window).on('resize', function(){
 	h = $(window).height();
