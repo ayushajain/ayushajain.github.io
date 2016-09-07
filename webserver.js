92ef938c5177a63029306a271bfee4fc7cfbdfb6
Ayush Jain <ayushajain@gmail.com>
2016-09-06 22:33:25 -0700

    fixed deprecated express command

--- webserver.js
+++ webserver.js
@@ -6,10 +6,10 @@ app.use('/' , express.static(__dirname + '/'));
 
 
 app.get('/about', function(req, res){
-    res.sendFile('about.html');
+    res.sendfile('about.html');
 });
 app.get('/projects', function(req, res){
-    res.sendFile('projects.html');
+    res.sendfile('projects.html');
 });
 app.listen(8080, function(){
     console.log("listenting on http://localhost:8080");
